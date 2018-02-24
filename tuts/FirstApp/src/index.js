3335bbb837961388c3a0d788b701d1983d740a11
kentenroth <kentencroth@yahoo.com>
2018-02-23 21:04:29 -0600

    hooked up API
    Hooked up the API to display video thumbnails and video titles in a list.

--- tuts/FirstApp/src/index.js
+++ tuts/FirstApp/src/index.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list'
+const API_KEY = 'AIzaSyAlvCiZB9qJWSV7uvKbl7Fmas39LZKHgiI';
 
-const API_KEY = 'AIzaSyAlvCiZB9qJWSV7uvKbl7Fmas39LZKHgiI'
 
 
+class App extends Component {
+    constructor(props) {
+        super(props);
 
-const App = () => {
-    return (
-    <div>
-        <SearchBar />
-    </div>
-    );
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: ''}, (data) => {
+            this.setState({ videos: data });
+        });
+    }
+
+    render() {
+        return (
+        <div>
+            <SearchBar />
+            <VideoList videos={this.state.videos} />
+        </div>
+        );
+    }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
