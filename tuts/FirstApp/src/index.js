1ae06bfc66866b71898197cdb6d89f15775ebcef
kentenroth <kentencroth@yahoo.com>
2018-02-24 14:17:02 -0600

    hooking stuff up
    hooking some of the stuff up and cleaning up how the page looks

--- tuts/FirstApp/src/index.js
+++ tuts/FirstApp/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list'
+import VideoDetail from './components/video_detail'
 const API_KEY = 'AIzaSyAlvCiZB9qJWSV7uvKbl7Fmas39LZKHgiI';
 
 
@@ -11,18 +12,33 @@ class App extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { videos: [] };
+        this.state = { 
+            videos: [], 
+            selectedVideo: null
+        };
 
+        this.videoSearch('')
+    }
+
+    videoSearch(term) {
         YTSearch({key: API_KEY, term: ''}, (data) => {
-            this.setState({ videos: data });
+            this.setState({ 
+                videos: data,
+                selectedVideo: data[0]
+             });
         });
     }
+
+
 
     render() {
         return (
         <div>
-            <SearchBar />
-            <VideoList videos={this.state.videos} />
+            <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+            <VideoDetail video={this.state.selectedVideo} />
+            <VideoList 
+            onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+            videos={this.state.videos} />
         </div>
         );
     }
