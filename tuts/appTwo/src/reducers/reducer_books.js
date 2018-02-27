fd5ed2edc70000fee42b297d2dc056d3ebe90fca
kentenroth <kentencroth@yahoo.com>
2018-02-26 19:56:04 -0600

    Redux setup
    Starting on reducers.

---
+++ tuts/appTwo/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        {title: 'Crushing It'},
+        {title: 'The War of Art'},
+        {title: 'The Gold Standard'},
+        {title: 'Ready Player One'}
+    ]
+}
