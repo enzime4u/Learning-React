49774ffec61bf347ec93f66fba234830cec2ff14
kentenroth <kentencroth@yahoo.com>
2018-02-28 21:04:39 -0600

    reducers
    I want to read more about reducers and containers.  This didnt fully set in and just has not clicked.

--- tuts/appTwo/src/reducers/reducer_books.js
+++ tuts/appTwo/src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
     return [
-        {title: 'Crushing It'},
-        {title: 'The War of Art'},
-        {title: 'The Gold Standard'},
-        {title: 'Ready Player One'}
+        {title: 'Crushing It', pages: 263 },
+        {title: 'The War of Art', pages: 165 },
+        {title: 'The Gold Standard', pages: 194 },
+        {title: 'Ready Player One', pages: 372 }
     ]
 }
