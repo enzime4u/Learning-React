74b43d8e7db64ce1f6f1f8303fab59457031545e
kentenroth <kentencroth@yahoo.com>
2018-03-04 21:11:53 -0600

    reducers
    reducers

--- tuts/weather/src/actions/index.js
+++ tuts/weather/src/actions/index.js
@@ -1,1 +1,19 @@
-const API_KEY = process.env.REACT_APP_WEATHER_API_KEY
+import axios from 'axios'
+
+
+const API_KEY = 'e33c3048cfd891ee5941f074a01eccd7'
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
--- tuts/weather/src/containers/search_bar.js
+++ tuts/weather/src/containers/search_bar.js
@@ -1,26 +1,29 @@
-import React, { Component } from 'react' ;
-import API_KEY from "../index"
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux'
+import { fetchWeather } from '../actions/index'
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
     constructor(props) {
         super(props);
 
         this.state = { term: '' };
 
         this.onInputChange = this.onInputChange.bind(this);
+        this.onFormSubmit = this.onFormSubmit.bind(this);
 
-        console.log("my key" + API_KEY)
     }
 
     onInputChange(event) {
-        this.setState({ term: event.target.value});
-
-        console.log("my key" + API_KEY)
+        this.setState({ term: event.target.value });
+        
     }
 
     onFormSubmit(event) {
-        console.log(API_KEY)
         event.preventDefault();
+
+        this.props.fetchWeather(this.state.term);
+        this.setState({ term: '' });
     }
 
     render() {
@@ -30,8 +33,7 @@ export default class SearchBar extends Component {
                     placeholder="Get a five-day forcast in your favorite cities"
                     className="form-control"
                     value={this.state.term}
-                    onChange={this.onInputChange}
-                />
+                    onChange={this.onInputChange} />
                 <span className="input-group-btn">
                     <button type="submit" className="btn btn-secondary">Submit</button>
                 </span>
@@ -39,3 +41,9 @@ export default class SearchBar extends Component {
         );
     }
 }
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
