21d4aa439f93ec3143613c41eb6114b39483cbcf
kentenroth <kentencroth@yahoo.com>
2018-03-06 18:58:56 -0600

    API
    Hooked API up

--- tuts/weather/src/actions/index.js
+++ tuts/weather/src/actions/index.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const API_KEY = 'e33c3048cfd891ee5941f074a01eccd7'
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}&units=imperial`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
