c2f1705f0d30838434bdc309bdfaf934a978fe43
kentenroth <kentencroth@yahoo.com>
2018-03-11 09:24:03 -0500

    learning
    Going over the basics

--- tuts/appThree/public/scripts/app.js
+++ tuts/appThree/public/scripts/app.js
@@ -1,12 +1,54 @@
 'use strict';
 
-console.log('App.js is running!');
-
 var template = React.createElement(
-  'h1',
-  null,
-  'Indecision App'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Indecision App'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'This is some info'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item One'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item Two'
+        )
+    )
 );
+
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Kent Roth'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: 31'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: Iowa'
+    )
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- tuts/appThree/src/app.js
+++ tuts/appThree/src/app.js
@@ -1,6 +1,23 @@
-console.log('App.js is running!')
 
-var template = <h1>Indecision App</h1>;
+var template = (
+    <div>
+        <h1>Indecision App</h1>
+        <p>This is some info</p>
+        <ol>
+            <li>Item One</li>
+            <li>Item Two</li>
+        </ol>
+    </div>
+);
+
+var templateTwo = (
+    <div>
+        <h1>Kent Roth</h1>
+        <p>Age: 31</p>
+        <p>Location: Iowa</p>
+    </div>
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
