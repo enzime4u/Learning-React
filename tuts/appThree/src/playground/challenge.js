3de27c133bab104088d9c04e5cb676e64f2047c4
kentenroth <kentencroth@yahoo.com>
2018-03-15 20:30:33 -0500

    Challenge
    completed build it challenge

---
+++ tuts/appThree/src/playground/challenge.js
@@ -0,0 +1,33 @@
+let visibility = false;
+const toggleVisibility = () => {
+    visibility = !visibility;
+    render();
+};
+
+const render = () => {
+    const jsx = (
+        <div>
+            <h1>Visibility Toggle</h1>
+            <button onClick={toggleVisibility}>
+                {visibility ? 'Hide Details' : 'Show Details'}
+            </button>
+            {visibility && (
+                <div>
+                    <p>You can see this!</p>
+                </div>
+
+            )}
+        </div>
+    );
+    ReactDOM.render(jsx, document.getElementById('app'));
+};
+
+render();
+
+
+
+
+
+
+
+
--- tuts/appThree/src/playground/es6-let-const.js
+++ tuts/appThree/src/playground/es6-let-const.js
@@ -48,4 +48,4 @@ const renderCounterApp = () => {
     ReactDOM.render(templateTwo, appRoot);
 };
 
-renderCounterApp()
+renderCounterApp();
